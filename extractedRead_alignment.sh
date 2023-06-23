1bad04987433062e18582a7f4e04c45c87fbcf7b
bridlin <bridlinbarckmann@gmail.com>
2023-06-23 10:40:05 +0200

    completed alignemnet sh

--- extractedRead_alignment.sh
+++ extractedRead_alignment.sh
@@ -11,7 +11,16 @@
 #SBATCH --mem 64GB
 
 
- module load bowtie2/2.5.1
+module load bowtie2/2.5.1
+module load  samtools/1.13
+
+source WGS_metagenomic_analysis/config-align.txt
 
 
- 
+for x in "${input_list[@]}"; do
+bowtie2 -x $genome -f -p 8  -1 $inputlist\\.1.fq  -2 $inputlist\\.2.fq  -S $inputlist\\_aligned.sam &&
+samtools view -S -b $inputlist\_aligned.sam > $inputlist\_aligned.bam  &&
+samtools sort  $inputlist\_aligned.bam  -o  $inputlist\_aligned_sorted.bam  &&
+samtools index $inputlist\_aligned_sorted.bam   &&
+rm -f $inputlist\\_aligned.sam &&
+rm -f $inputlist\_aligned.bam ; done
