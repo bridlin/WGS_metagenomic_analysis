3b4f86265ebcee5f37ec8abb91521fa905b74331
bridlin <bridlinbarckmann@gmail.com>
2023-06-23 10:24:50 +0200

    add sh file to alin extracted reads

---
+++ extractedRead_alignment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user b-barckmann@chu-montpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpus-per-task 6
+#SBATCH --mem 64GB
+
+
+ module load bowtie2/2.5.1
+
+
+ 
