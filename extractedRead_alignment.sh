37561facb7a95ff2261e2855258d29a516bd8749
bridlin <bridlinbarckmann@gmail.com>
2023-06-23 11:35:43 +0200

    added echo to test the config output

--- extractedRead_alignment.sh
+++ extractedRead_alignment.sh
@@ -16,6 +16,9 @@ module load  samtools/1.13
 
 source WGS_metagenomic_analysis/config_align
 
+echo $read_directory
+echo $inputlist
+echo $genome
 
 for x in "${input_list[@]}"; do
 bowtie2 -x $genome -f -p 8  -1 $read_directory\$inputlist\.1.fq  -2 $read_directory\$inputlist\.2.fq  -S $read_directory\$inputlist\_aligned.sam &&
