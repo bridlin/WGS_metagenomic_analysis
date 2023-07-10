73fc02a033653e170f9e161dae0bad115a8cf46b
bridlin <bridlinbarckmann@gmail.com>
2023-07-10 14:04:19 +0200

    changed output dir for fastqc

--- ReadTrim_AlignmentHS_Kraken2.sh
+++ ReadTrim_AlignmentHS_Kraken2.sh
@@ -29,15 +29,15 @@ fastqc $directory/$x\L001_R1_001.fastq.gz --outdir $output_dir &&
 fastqc $directory/$x\L001_R2_001.fastq.gz --outdir $output_dir &&
 cutadapt  -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  -o $directory/$x\L001_R1_001_3trimmed.fastq.gz -p $directory/$x\L001_R2_001_3trimmed.fastq.gz  $directory/$x\L001_R1_001.fastq.gz  $directory/$x\L001_R2_001.fastq.gz --minimum-length 40 > $directory/$x\report.txt &&
 trimmomatic PE -threads 4 -trimlog $directory/$x\trim $directory/$x\L001_R1_001_3trimmed.fastq.gz $directory/$x\L001_R2_001_3trimmed.fastq.gz $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz   $directory/$x\L001_R1_001_3trimmed_q20_un.fastq.gz $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz  $directory/$x\L001_R2_001_3trimmed_q20_un.fastq.gz SLIDINGWINDOW:4:20 MINLEN:40 &&
-fastqc $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz &&
-fastqc $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz &&
+fastqc $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz --outdir $output_dir &&
+fastqc $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz --outdir $output_dir &&
 bowtie2 -x ../../bank/bowtie2/Homo_sapiens.GRCh38.dna.toplevel -1 $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz -2 $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz  --un-conc $directory/$x\nonhuman_reads.fastq -S $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam 2> $directory/$x\.log &&
 samtools view -S -b $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam > $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
 samtools sort $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam -o $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
 samtools index $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
 rm -f  $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam &&
 rm -f  $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
-kraken2 --db $kraken2_db --threads 8 --minimum-hit-groups 3  --report-minimizer-data --report $kraken_output_dir/$x\.k2report  --paired $directory/$x\nonhuman_reads.1.fastq $directory/$x\nonhuman_reads.2.fastq > $kraken_output_dir/$x\.kraken2 &&
-kraken2 --db $kraken2_db_2 --threads 8 --minimum-hit-groups 3  --report-minimizer-data --report $kraken_output_dir_2/$x\.k2report  --paired $directory/$x\nonhuman_reads.1.fastq $directory/$x\nonhuman_reads.2.fastq > $kraken_output_dir_2/$x\.kraken2; done
+kraken2 --db $kraken2_db --threads 8 --minimum-hit-groups 3  --report-minimizer-data --report $output_dir/$kraken_output_dir/$x\.k2report  --paired $directory/$x\nonhuman_reads.1.fastq $directory/$x\nonhuman_reads.2.fastq > $output_dir/$kraken_output_dir/$x\.kraken2 &&
+kraken2 --db $kraken2_db_2 --threads 8 --minimum-hit-groups 3  --report-minimizer-data --report $output_dir/$kraken_output_dir_2/$x\.k2report  --paired $directory/$x\nonhuman_reads.1.fastq $directory/$x\nonhuman_reads.2.fastq > $output_dir/$kraken_output_dir_2/$x\.kraken2; done
 
 multiqc $directory $output_dir $kraken_output_dir $kraken_output_dir_2
