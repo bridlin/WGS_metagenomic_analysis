2d797969bdee6fe7028ee6add3f0a925907b213d
bridlin <bridlinbarckmann@gmail.com>
2023-07-10 09:55:56 +0200

    adapted the read extraction -- I need to put the config files in the gitignore

--- read-extraction.sh
+++ read-extraction.sh
@@ -15,4 +15,4 @@ module load python/3.9
 source WGS_metagenomic_analysis/config_readextraction
 
 for x in "${input_list[@]}"; do for id in "${id_list[@]}"; do 
-python KrakenTools-master/extract_kraken_reads.py -k $kraken_results/$x\.kraken2 --include-children -s $read_directory/$x\L001_R1_001_3trimmed_q20.fastq.gz -s2 $read_directory/$x\L001_R2_001_3trimmed_q20.fastq.gz -t $id\  -r $kraken_results/$x\.k2report -o $x\.tid$id\.1.fa  -o2 $x\.tid$id\.2.fa  ; done ; done
+python KrakenTools-master/extract_kraken_reads.py -k $kraken_results/$x\.kraken2 --include-children -s $read_directory/$x\nonhuman_reads.1.fastq -s2 $read_directory/$x\nonhuman_reads.2.fastq -t $id\  -r $kraken_results/$x\.k2report -o $x\.tid$id\.1.fa  -o2 $x\.tid$id\.2.fa  ; done ; done
