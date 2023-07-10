c132c28361c04bbacf9144ea1ee44927064e9718
bridlin <bridlinbarckmann@gmail.com>
2023-07-10 10:11:52 +0200

    added directory for trimmomatic report and added report output for cutadapt

--- ReadTrim_AlignmentHS_Kraken2.sh
+++ ReadTrim_AlignmentHS_Kraken2.sh
@@ -24,16 +24,16 @@ source WGS_metagenomic_analysis/config.yml
 
 mkdir $output_dir
 for x in "${input_list[@]}"; do
-# fastqc $directory/$x\L001_R1_001.fastq.gz &&
-# fastqc $directory/$x\L001_R2_001.fastq.gz &&
-# cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  -o $directory/$x\L001_R1_001_3trimmed.fastq.gz -p $directory/$x\L001_R2_001_3trimmed.fastq.gz  $directory/$x\L001_R1_001.fastq.gz  $directory/$x\L001_R2_001.fastq.gz --minimum-length 40 &&
-# trimmomatic PE -threads 4 -trimlog $x\trim $directory/$x\L001_R1_001_3trimmed.fastq.gz $directory/$x\L001_R2_001_3trimmed.fastq.gz $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz   $directory/$x\L001_R1_001_3trimmed_q20_un.fastq.gz $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz  $directory/$x\L001_R2_001_3trimmed_q20_un.fastq.gz SLIDINGWINDOW:4:20 MINLEN:40 &&
-# fastqc $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz &&
-# fastqc $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz &&
-# bowtie2 -x ../../bank/bowtie2/Homo_sapiens.GRCh38.dna.toplevel -1 $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz -2 $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz  --un-conc $directory/$x\nonhuman_reads.fastq -S $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam &&
-# samtools view -S -b $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam > $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
-# samtools sort $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam -o $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
-# samtools index $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
-# rm -f  $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam &&
-# rm -f  $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
+fastqc $directory/$x\L001_R1_001.fastq.gz &&
+fastqc $directory/$x\L001_R2_001.fastq.gz &&
+cutadapt  -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  -o $directory/$x\L001_R1_001_3trimmed.fastq.gz -p $directory/$x\L001_R2_001_3trimmed.fastq.gz  $directory/$x\L001_R1_001.fastq.gz  $directory/$x\L001_R2_001.fastq.gz --minimum-length 40 2> $directory/$x\_report.txt &&
+trimmomatic PE -threads 4 -trimlog $directory/$x\trim $directory/$x\L001_R1_001_3trimmed.fastq.gz $directory/$x\L001_R2_001_3trimmed.fastq.gz $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz   $directory/$x\L001_R1_001_3trimmed_q20_un.fastq.gz $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz  $directory/$x\L001_R2_001_3trimmed_q20_un.fastq.gz SLIDINGWINDOW:4:20 MINLEN:40 &&
+fastqc $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz &&
+fastqc $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz &&
+bowtie2 -x ../../bank/bowtie2/Homo_sapiens.GRCh38.dna.toplevel -1 $directory/$x\L001_R1_001_3trimmed_q20.fastq.gz -2 $directory/$x\L001_R2_001_3trimmed_q20.fastq.gz  --un-conc $directory/$x\nonhuman_reads.fastq -S $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam &&
+samtools view -S -b $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam > $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
+samtools sort $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam -o $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
+samtools index $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
+rm -f  $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam &&
+rm -f  $directory/$x\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
 kraken2 --db $kraken2_db --threads 8 --minimum-hit-groups 3  --report-minimizer-data --report $output_dir/$x\.k2report  --paired $directory/$x\nonhuman_reads.1.fastq $directory/$x\nonhuman_reads.2.fastq > $output_dir/$x\.kraken2 ; done
