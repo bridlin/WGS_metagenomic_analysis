37f0e072456725cc77f8c3b74b37b25cf863decc
bridlin <bridlinbarckmann@gmail.com>
2023-08-17 17:07:28 +0200

    working on G_toxoID extraction

--- G_taxoID_extraction.py
+++ G_taxoID_extraction.py
@@ -1,4 +1,4 @@
-#import pandas as pd
+import pandas as pd
 import os
 
 
@@ -12,11 +12,19 @@ def get_sample_names(file_path):
             if os.path.splitext(names)[1] == ".k2report":
                  sample_names.append(os.path.splitext(names)[0])
     return sample_names    
-       
+
+
+def read_report(report):
+    df_report = pd.read_table(report)
+    return(df_report)
  
 def main():
     file_path = '../../kraken2-results_run11_5prime-trimmed/PlusPF/'  # file path
     print(get_sample_names(file_path))
+
+    report = '../../kraken2-results_run11_5prime-trimmed/PlusPF/MC6A_S1_.k2report'
+    print(read_report(report))
+
 
 if __name__ == "__main__":
     main()
