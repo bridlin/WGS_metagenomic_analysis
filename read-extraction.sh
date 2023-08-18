e25a78157586e4ebf8c1e05a153a2836990e97ca
bridlin <bridlinbarckmann@gmail.com>
2023-08-18 16:43:32 +0200

    adapted source in read-extraction to the new config file

--- read-extraction.sh
+++ read-extraction.sh
@@ -12,7 +12,8 @@
 
 module load python/3.9
 
-source WGS_metagenomic_analysis/config_readextraction
+source WGS_metagenomic_analysis/config.yml
+source WGS_metagenomic_analysis/config_readextraction.txt
 
 for x in "${input_list[@]}"; do for id in "${id_list[@]}"; do 
 python KrakenTools-master/extract_kraken_reads.py -k $kraken_results/$x\.kraken2 --include-children -s $read_directory/$x\nonhuman_reads.1.fastq -s2 $read_directory/$x\nonhuman_reads.2.fastq -t $id\  -r $kraken_results/$x\.k2report -o $x\.tid$id\.1.fa  -o2 $x\.tid$id\.2.fa  ; done ; done
