1332b4d3f6bc7c5b6f6c2c2819b9ed3036e6d8d8
bridlin <bridlinbarckmann@gmail.com>
2023-08-18 16:45:35 +0200

    changed variable name from X to sample

--- read-extraction.sh
+++ read-extraction.sh
@@ -15,5 +15,5 @@ module load python/3.9
 source WGS_metagenomic_analysis/config.yml
 source WGS_metagenomic_analysis/config_readextraction.txt
 
-for x in "${input_list[@]}"; do for id in "${id_list[@]}"; do 
-python KrakenTools-master/extract_kraken_reads.py -k $kraken_results/$x\.kraken2 --include-children -s $read_directory/$x\nonhuman_reads.1.fastq -s2 $read_directory/$x\nonhuman_reads.2.fastq -t $id\  -r $kraken_results/$x\.k2report -o $x\.tid$id\.1.fa  -o2 $x\.tid$id\.2.fa  ; done ; done
+for sample in "${input_list[@]}"; do for id in "${id_list[@]}"; do 
+python KrakenTools-master/extract_kraken_reads.py -k $kraken_results/$sample\.kraken2 --include-children -s $read_directory/$sample\nonhuman_reads.1.fastq -s2 $read_directory/$sample\nonhuman_reads.2.fastq -t $id\  -r $kraken_results/$sample\.k2report -o $sample\.tid$id\.1.fa  -o2 $sample\.tid$id\.2.fa  ; done ; done
