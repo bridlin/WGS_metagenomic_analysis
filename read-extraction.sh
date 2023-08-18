f837afee6c6ff8bda7d3f0330227e5886c16d6f9
bridlin <bridlinbarckmann@gmail.com>
2023-08-18 16:56:37 +0200

    config.yaml and read-extraction adapted to manip

--- read-extraction.sh
+++ read-extraction.sh
@@ -15,5 +15,7 @@ module load python/3.9
 source WGS_metagenomic_analysis/config.yml
 source WGS_metagenomic_analysis/config_readextraction.txt
 
-for sample in "${input_list[@]}"; do for id in "${id_list[@]}"; do 
-python KrakenTools-master/extract_kraken_reads.py -k $kraken_results/$sample\.kraken2 --include-children -s $read_directory/$sample\nonhuman_reads.1.fastq -s2 $read_directory/$sample\nonhuman_reads.2.fastq -t $id\  -r $kraken_results/$sample\.k2report -o $sample\.tid$id\.1.fa  -o2 $sample\.tid$id\.2.fa  ; done ; done
+for sample in "${input_list[@]}"; do for id in "${$sample\[@]}"; do 
+python KrakenTools-master/extract_kraken_reads.py -k $output_dir/$kraken_output_dir/$sample\.kraken2 --include-children -s $read_directory/$sample\nonhuman_reads.1.fastq -s2 $read_directory/$sample\nonhuman_reads.2.fastq -t $id\  -r $output_dir/$kraken_output_dir/$sample\.k2report -o $sample\.tid$id\.1.fa  -o2 $sample\.tid$id\.2.fa  ; done ; done
+
+
