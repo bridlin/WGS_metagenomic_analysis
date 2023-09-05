23408a10e704ebf0ebdd18826b55b23c97d80be9
bridlin <bridlinbarckmann@gmail.com>
2023-09-05 10:55:45 +0200

    adapted cut to new path

--- auto_blast.sh
+++ auto_blast.sh
@@ -15,6 +15,6 @@ module load blast/2.14.0
 
 #kraken_file_path=../kraken2-results_run12_5prime-trimmed/EuPathDB48/
 mkdir ../kraken2-results_run12_5prime-trimmed/EuPathDB48/blast_result
-for files in ../kraken2-results_run12_5prime-trimmed/EuPathDB48/extracted_reads/*.1.fa ; do file=$( echo $files | cut -d / -f 4) && echo $files && echo $file  && echo $file\_blast && blastn -db nt -query $files -out $file\_blast  -max_target_seqs 5 -max_hsps 5   -outfmt "7 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" -remote && mv $file\_blast ../kraken2-results_run9_5prime-trimmed/EuPathDB48/blast_result ; done
+for files in ../kraken2-results_run12_5prime-trimmed/EuPathDB48/extracted_reads/*.1.fa ; do file=$( echo $files | cut -d / -f 5) && echo $files && echo $file  && echo $file\_blast && blastn -db nt -query $files -out $file\_blast  -max_target_seqs 5 -max_hsps 5   -outfmt "7 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" -remote && mv $file\_blast ../kraken2-results_run9_5prime-trimmed/EuPathDB48/blast_result ; done
 
 
