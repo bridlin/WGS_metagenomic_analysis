c310539cafddf6594d625840159002e128844984
bridlin <bridlinbarckmann@gmail.com>
2023-11-15 14:37:26 +0100

    changed in individual read extraction script

--- read-extraction.sh
+++ read-extraction.sh
@@ -12,10 +12,21 @@
 
 module load python/3.9
 
-source WGS_metagenomic_analysis/config.yml
+id_list=("475")
+input_list=("3737_S4_" "3735_S3_")
+kraken_results=kraken2-results_run8/PlusPF
+read_directory=run8_nonhuman-reads
+output_dir=kraken2-results_run14_5prime-trimmed
+db=PlusPF
+#db_2=EuPathDB48
+
+
+
+
+
 source WGS_metagenomic_analysis/config_readextraction.txt
 
 for sample in "${input_list[@]}"; do for id in "${$sample[@]}"; do 
-python KrakenTools-master/extract_kraken_reads.py -k $output_dir/$kraken_output_dir/$sample\.kraken2 --include-children -s $read_directory/$sample\nonhuman_reads.1.fastq -s2 $read_directory/$sample\nonhuman_reads.2.fastq -t $id\  -r $output_dir/$kraken_output_dir/$sample\.k2report -o $sample\.tid$id\.1.fa  -o2 $sample\.tid$id\.2.fa  ; done ; done
+python KrakenTools-master/extract_kraken_reads.py -k $output_dir/$db/$sample\_$db.kraken2 --include-children -s $read_directory/$sample\nonhuman_reads_5trimmed.1.fastq -s2 $read_directory/$sample\nonhuman_reads_5trimmed.2.fastq -t $id\  -r $output_dir/$db/$sample\_$db.k2report -o $sample\.tid$id\.1.fa  -o2 $sample\.tid$id\.2.fa  ; done ; done
 
 
