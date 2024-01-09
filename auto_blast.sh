7ccf381d3cec3edd27c86f80ae1c55f5d0303c49
bridlin <bridlinbarckmann@gmail.com>
2024-01-09 09:46:53 +0100

    adapted auto blast to new experiment

--- auto_blast.sh
+++ auto_blast.sh
@@ -13,8 +13,8 @@
 module load blast/2.14.0
 
 
-#kraken_file_path=../kraken2-results_run14_5prime-trimmed/PlusPF/
-mkdir ../kraken2-results_run14_5prime-trimmed/PlusPF/blast_result
+#kraken_file_path=../kraken2-results_run16_5prime-trimmed/PlusPF/
+mkdir ../kraken2-results_run16_5prime-trimmed/PlusPF/blast_result
 for files in ../kraken2-results_run14_5prime-trimmed/PlusPF/*.1.fa ; do file=$( echo $files | cut -d / -f 4) && echo $files && echo $file  && echo $file\_blast && blastn -db nt -query $files -out $file\_blast  -max_target_seqs 5 -max_hsps 5   -outfmt "7 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" -remote && mv $file\_blast ../kraken2-results_run14_5prime-trimmed/PlusPF/blast_result ; done
 
 
