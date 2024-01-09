e874e5041628d92c4c5a61a2ccbd1a2e5b87197e
bridlin <bridlinbarckmann@gmail.com>
2024-01-09 09:43:39 +0100

    created shell script that runs the Python extract reads script

---
+++ run_python_extract_kraken_reads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user b-barckmann@chu-montpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpus-per-task 6
+#SBATCH --mem 64GB
+
+module load python/3.9
+
+python3 WGS_metagenomic_analysis/auto_read-Extraction.py 'fastq_run16/' 'kraken2-results_run16_5prime-trimmed/PlusPF/'
+
+python3 WGS_metagenomic_analysis/auto_read-Extraction.py 'fastq_run16/' 'kraken2-results_run16_5prime-trimmed/EuPathDB48/'
