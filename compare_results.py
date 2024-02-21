69f5c882904a555d435739375331bc4ae0a527b0
bridlin <bridlinbarckmann@gmail.com>
2024-02-21 16:12:10 +0100

    function to read in the taxoID df

--- compare_results.py
+++ compare_results.py
@@ -9,9 +9,12 @@ import os
 #### Methodes ####
 
 def get_sample_names(results_path):
-    print(results_path)
+    #print(results_path)
     sample_names = []
     for root, dirs, files in os.walk(results_path):
+        print(root)
+        print(dirs)
+        print(files)
         for names in files:
             #print(files)
             #print(names)
@@ -21,22 +24,24 @@ def get_sample_names(results_path):
 
 def strip_samplenames(sample_name,kraken_file_path):
     substring = kraken_file_path.split('/')
+    print(substring)
     striped_name = sample_name.split(substring[-2])
     return striped_name[0]
 
 
-def read_G_taxoIDs(G_taxoIDs, column_names):
-    df_report = pd.read_table(G_taxoIDs, names=column_names)
-    return df_report
 
 
 
-
-
-
-
-
-
+def read_G_taxoIDs(results_path):
+    for root, dirs, files in os.walk(results_path):
+        for file in files:
+            if file == "G_TaxoIDs_per_sample.tsv" :
+                G_taxo = results_path +'/'+ file
+                print(G_taxo)
+                df_report = pd.read_table(G_taxo,index_col=0)
+            else :
+                print('there is no G-Taxo file at the indicated result path')
+    return df_report    
 
 
 
@@ -54,8 +59,10 @@ def main():
     results_path = '../../kraken2-results_run15_5prime-trimmed/EuPathDB48'
 
     print(get_sample_names(results_path))
-    print(strip_samplenames(sample_names,results_path))
+    #print(strip_samplenames(get_sample_names(results_path),results_path))
 
+    column_names_G_taxo = ['sample','taxoID','name','reads']
+    print(read_G_taxoIDs(results_path))
 
 
 
