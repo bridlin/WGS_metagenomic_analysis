a522d801a3648c603b0cd1cbfd9a0ba943a6f9be
bridlin <bridlinbarckmann@gmail.com>
2024-04-05 11:58:25 +0200

    make the script more readable

--- auto_blast.sh
+++ auto_blast.sh
@@ -16,13 +16,37 @@ module load blast/2.14.0
 #kraken_file_path=../kraken2-results_run16_5prime-trimmed/PlusPF/
 
 mkdir ../kraken2-results_run21_5prime-trimmed/EuPathDB48/blast_result
-for files in ../kraken2-results_run21_5prime-trimmed/EuPathDB48/*.1.fa ; do file=$( echo $files | cut -d / -f 4) && echo $files && echo $file  && echo $file\_blast && blastn -db nt -query $files -out $file\_blast  -max_target_seqs 5 -max_hsps 5   -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" -remote \
-&& mv $file\_blast ../kraken2-results_run21_5prime-trimmed/EuPathDB48/blast_result ; done
+for files in ../kraken2-results_run21_5prime-trimmed/EuPathDB48/*.1.fa ; do \
+    file=$( echo $files | cut -d / -f 4) && \
+    echo $files && \
+    echo $file  && \
+    echo $file\_blast && \
+    blastn \
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote &&\
+mv $file\_blast ../kraken2-results_run21_5prime-trimmed/EuPathDB48/blast_result ; done
 
 
 mkdir ../kraken2-results_run21_5prime-trimmed/PlusPF/blast_result
-for files in ../kraken2-results_run21_5prime-trimmed/PlusPF/*.1.fa ; do file=$( echo $files | cut -d / -f 4) && echo $files && echo $file  && echo $file\_blast && blastn -db nt -query $files -out $file\_blast  -max_target_seqs 5 -max_hsps 5   -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" -remote \
-&& mv $file\_blast ../kraken2-results_run21_5prime-trimmed/PlusPF/blast_result ; done
+for files in ../kraken2-results_run21_5prime-trimmed/PlusPF/*.1.fa ; do \
+    file=$( echo $files | cut -d / -f 4) && \
+    echo $files && \
+    echo $file  && \
+    echo $file\_blast && \
+    blastn \
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote && \
+mv $file\_blast ../kraken2-results_run21_5prime-trimmed/PlusPF/blast_result ; done
 
 
 
