9848805f82bedae4db51df933474d771782e02e8
bridlin <bridlinbarckmann@gmail.com>
2024-05-13 12:17:33 +0200

    added convertion of blastresults in dataframe

--- compare_results.py
+++ compare_results.py
@@ -50,9 +50,13 @@ def read_blast_result(results_path):
 def blast_result_as_df(taxoid,sample_name,result_path):
     blastfile = result_path + '/blast_result_6/' + sample_name + '.tid' + str(taxoid) + '.1.fa_blast'       
     dict_blast = parse_tabular_blast_results(blastfile)
-    #print(dict_blast)
-    # df_blast = pd.DataFrame.from_dict(dict_blast, orient='index').stack().apply(pd.Series).stack().apply(pd.Series)
-    return(dict_blast)
+    if not dict_blast:
+        return None
+    else :
+        df_blast = pd.DataFrame.from_dict(dict_blast, orient='index').stack().apply(pd.Series).stack().apply(pd.Series)
+        df_blast["taxoID_kraken2"] = taxoid
+        df_blast["sample"] = sample_name
+    return(df_blast)
    
 #### Main ####
 def main():
@@ -70,30 +74,24 @@ def main():
     df_G_taxo = read_G_taxoIDs(results_path)
     print(df_G_taxo)
     
-    #for name, values in df_G_taxo.items():
-        #print(name)
-        #print(values)
-        #print('{name}: {value}'.format(name=name, value=values[0]))
-        # if name == 'taxoID':
-        #     taxoid=values
-        #     print(taxoid)
-        #     print(str(taxoid))
-        #     print('../../run15_WGS_test/kraken2-results_run15_5prime-trimmed/EuPathDB48/blast_result_6/1528T_S2_EuPathDB48.tid' + str(taxoid) + '.1.fa_blast')
+
     for sample in get_sample_names(results_path):
-        for column in df_G_taxo.columns[1:2]:
-            for taxoid in (df_G_taxo[column]):
-                print(taxoid) 
-                print('../../run15_WGS_test/kraken2-results_run15_5prime-trimmed/EuPathDB48/blast_result_6/1528T_S2_EuPathDB48.tid' + str(taxoid) + '.1.fa_blast')
-                print(results_path)
-                print(sample)   
-                print(blast_result_as_df(taxoid,sample,results_path))
+        print(sample)
+        taxoids = df_G_taxo.loc[df_G_taxo['sample'] == sample, 'taxoID']
+        #print(taxoids)
+        for taxoid in taxoids:
+            print(taxoid)
+            print(blast_result_as_df(taxoid,sample,results_path))
+
+
 
     
-    
-    # dict_blast = parse_tabular_blast_results('../../run15_WGS_test/kraken2-results_run15_5prime-trimmed/EuPathDB48/blast_result_6/1528T_S2_EuPathDB48.tid55193.1.fa_blast')
-    # #print(dict_blast)
-    # df_blast = pd.DataFrame.from_dict(dict_blast, orient='index').stack().apply(pd.Series).stack().apply(pd.Series)
-    # print(df_blast)
+
+
+
+
+
+   
 
     
 
