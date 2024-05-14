b2cc20d50e3e1486e0f98f1b101596d0197cb82c
bridlin <bridlinbarckmann@gmail.com>
2024-05-14 13:13:09 +0200

    added a bash script to run compare results

---
+++ run_compare_results.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user b-barckmann@chu-montpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpus-per-task 6
+#SBATCH --mem 64GB
+
+module load python/3.9
+
+python3 WGS_metagenomic_analysis/compare_results.py  '../../run15_WGS_test/kraken2-results_run15_5prime-trimmed/EuPathDB48/'
+
+python3 WGS_metagenomic_analysis/compare_results.py  '../../run15_WGS_test/kraken2-results_run15_5prime-trimmed/PlusPF/'
