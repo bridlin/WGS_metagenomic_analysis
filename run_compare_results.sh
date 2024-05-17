b29e450cbc3e317033e2c1de8e6bda607391acb5
bridlin <bridlinbarckmann@gmail.com>
2024-05-17 16:40:23 +0200

    added ete# module load to run comparison script

--- run_compare_results.sh
+++ run_compare_results.sh
@@ -11,7 +11,8 @@
 #SBATCH --mem 64GB
 
 module load python/3.9
+module load ete3/3.1.2
 
-python3 WGS_metagenomic_analysis/compare_results.py  'kraken2-results_run23_5prime-trimmed/EuPathDB48/'
+python3 WGS_metagenomic_analysis/compare_results.py  'kraken2-results_run24_5prime-trimmed/EuPathDB48/'
 
-python3 WGS_metagenomic_analysis/compare_results.py  'kraken2-results_run23_5prime-trimmed/PlusPF/'
+python3 WGS_metagenomic_analysis/compare_results.py  'kraken2-results_run24_5prime-trimmed/PlusPF/'
