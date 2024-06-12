e705d4342a3d643ccd5695f124f8de46419fea36
bridlin <bridlinbarckmann@gmail.com>
2024-06-12 15:33:44 +0200

    corrected result path

--- run_compare_results.sh
+++ run_compare_results.sh
@@ -14,12 +14,12 @@ module load python/3.9
 
 source WGS_metagenomic_analysis/config.txt
 
-eu_blast_result=kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result
-plus_blast_result=kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result
+eu_result=kraken2-results_$run\_5prime-trimmed/EuPathDB48
+plus_result=kraken2-results_$run\_5prime-trimmed/PlusPF
 
-echo $eu_blast_result
-echo $plus_blast_result
+echo $eu_result
+echo $plus_result
 
-python3 WGS_metagenomic_analysis/compare_results.py  $eu_blast_result
+python3 WGS_metagenomic_analysis/compare_results.py  $eu_result
 
-python3 WGS_metagenomic_analysis/compare_results.py  $plus_blast_result
+python3 WGS_metagenomic_analysis/compare_results.py  $plus_result
