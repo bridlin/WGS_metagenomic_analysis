1f44036819399b3661573bcd277121d00b337fdf
bridlin <bridlinbarckmann@gmail.com>
2024-08-05 16:29:19 +0200

    changes filepath varaibls

--- run_Readextraction_Blast_compareResults.sh
+++ run_Readextraction_Blast_compareResults.sh
@@ -40,8 +40,8 @@ python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_eu
 
 cd auto_blast_folder/
 
-mkdir ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result
-for files in ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/*.1.fa ; do \
+mkdir ../$kraken_eu\blast_result
+for files in ../$kraken_eu\*.1.fa ; do \
     file=$( echo $files | cut -d / -f 4) && \
     echo $files && \
     echo $file  && \
@@ -54,7 +54,7 @@ for files in ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/*.1.fa ; do \
         -max_hsps 5   \
         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
         -remote &&\
-mv $file\_blast ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result ; done
+mv $file\_blast ../$kraken_eu\blast_result ; done
 
 
 # mkdir ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result
