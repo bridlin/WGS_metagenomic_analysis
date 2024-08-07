a5c492c35c3367a76b4cc32d56e5cdd5f2813b16
bridlin <bridlinbarckmann@gmail.com>
2024-08-07 09:59:37 +0200

    changed line commands

--- run_Readextraction_Blast_compareResults.sh
+++ run_Readextraction_Blast_compareResults.sh
@@ -31,9 +31,9 @@ fastq=$run\_fastq
 
 ### run python script to extract 10 reads per genus from the kraken2 results
 
-python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_plus
+#python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_plus
 
-#python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_eu
+python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_eu
 
 
 ### run blast on the extracted reads
@@ -42,29 +42,12 @@ echo auto_blast
 
 cd auto_blast_folder/
 
-# mkdir ../$kraken_eu\blast_result
-# echo ../$kraken_eu\*.1.fa
-# for files in ../kraken2-results_$run\_5prime-trimmed_dedup/EuPathDB48/*.1.fa ; do \
-# #for files in ../{$kraken_eu}/*.1.fa ; do \
-#     echo $files && 
-#     file=$( echo $files | cut -d / -f 4) && 
-#     echo $file  && 
-#     echo $file\_blast && 
-#     blastn \
-#         -db nt \
-#         -query $files \
-#         -out $file\_blast  \
-#         -max_target_seqs 5 \
-#         -max_hsps 5   \
-#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-#         -remote && 
-# mv $file\_blast ../$kraken_eu\blast_result ; done
-
-
-mkdir ../$kraken_plus\blast_result
-for files in ../{$kraken_plus}*.1.fa ; do \
-    file=$( echo $files | cut -d / -f 4) && \
+mkdir ../$kraken_eu\blast_result
+echo ../$kraken_eu\*.1.fa
+f#or files in ../kraken2-results_$run\_5prime-trimmed_dedup/EuPathDB48/*.1.fa ; do \
+for files in ../"$kraken_eu"/*.1.fa ; do \
     echo $files && 
+    file=$( echo $files | cut -d / -f 4) && 
     echo $file  && 
     echo $file\_blast && 
     blastn \
@@ -74,13 +57,30 @@ for files in ../{$kraken_plus}*.1.fa ; do \
         -max_target_seqs 5 \
         -max_hsps 5   \
         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-        -remote && \
-mv $file\_blast ../$kraken_plus\blast_result ; done
+        -remote && 
+mv $file\_blast ../$kraken_eu\blast_result ; done
+
+
+# mkdir ../$kraken_plus\blast_result
+# for files in ../{$kraken_plus}*.1.fa ; do \
+#     file=$( echo $files | cut -d / -f 4) && \
+#     echo $files && 
+#     echo $file  && 
+#     echo $file\_blast && 
+#     blastn \
+#         -db nt \
+#         -query $files \
+#         -out $file\_blast  \
+#         -max_target_seqs 5 \
+#         -max_hsps 5   \
+#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+#         -remote && \
+# mv $file\_blast ../$kraken_plus\blast_result ; done
 
 cd ..
 
 ### run python script to compare the results of the blast with the kraken2 results
 
-#python3 WGS_metagenomic_analysis/compare_results.py  $kraken_eu
+python3 WGS_metagenomic_analysis/compare_results.py  $kraken_eu
 
-python3 WGS_metagenomic_analysis/compare_results.py  $kraken_plus
+#python3 WGS_metagenomic_analysis/compare_results.py  $kraken_plus
