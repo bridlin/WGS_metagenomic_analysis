263fa286d08f75aa5c99c3583ea95aca41425668
bridlin <bridlinbarckmann@gmail.com>
2024-08-07 10:48:24 +0200

    line command error

--- run_Readextraction_Blast_compareResults.sh
+++ run_Readextraction_Blast_compareResults.sh
@@ -33,7 +33,7 @@ fastq=$run\_fastq
 
 #python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_plus
 
-python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_eu
+#python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_eu
 
 
 ### run blast on the extracted reads
@@ -44,7 +44,7 @@ cd auto_blast_folder/
 
 mkdir ../$kraken_eu\blast_result
 echo ../$kraken_eu\*.1.fa
-f#or files in ../kraken2-results_$run\_5prime-trimmed_dedup/EuPathDB48/*.1.fa ; do \
+#for files in ../kraken2-results_$run\_5prime-trimmed_dedup/EuPathDB48/*.1.fa ; do \
 for files in ../"$kraken_eu"/*.1.fa ; do \
     echo $files && 
     file=$( echo $files | cut -d / -f 4) && 
