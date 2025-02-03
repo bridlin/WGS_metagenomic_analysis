76aa94f1efb013ebc907b0042d85c066dd9ebe74
bridlin <bridlinbarckmann@gmail.com>
2025-02-03 15:39:05 +0100

    autoblast with file check

--- auto_blast.sh
+++ auto_blast.sh
@@ -18,20 +18,19 @@ cd auto_blast_folder/
 
 mkdir ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result
 for files in ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/*.1.fa ; do \
-    if [ ! -f ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result/$file\_blast ] 
-    then
-        file=$( echo $files | cut -d / -f 4) && \
-        echo $files && \
-        echo $file  && \
-        echo $file\_blast && \
+    file=$( echo $files | cut -d / -f 4) && \
+    echo $files && \
+    echo $file  && \
+    echo $file\_blast && \
+    if [ ! -f ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result/$file\_blast ] ; then
         blastn \
-            -db nt \
-            -query $files \
-            -out $file\_blast  \
-            -max_target_seqs 5 \
-            -max_hsps 5   \
-            -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-            -remote 
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote 
     else 
         echo "blast is already done" && \
         echo $file\_blast
@@ -42,21 +41,26 @@ mv $file\_blast ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result 
 
 
 
-# mkdir ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result
-# for files in ../kraken2-results_$run\_5prime-trimmed/PlusPF/*.1.fa ; do \
-#     file=$( echo $files | cut -d / -f 4) && \
-#     echo $files && \
-#     echo $file  && \
-#     echo $file\_blast && \
-#     blastn \
-#         -db nt \
-#         -query $files \
-#         -out $file\_blast  \
-#         -max_target_seqs 5 \
-#         -max_hsps 5   \
-#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-#         -remote && \
-# mv $file\_blast ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result ; done
+mkdir ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result
+for files in ../kraken2-results_$run\_5prime-trimmed/PlusPF/*.1.fa ; do \
+    file=$( echo $files | cut -d / -f 4) && \
+    echo $files && \
+    echo $file  && \
+    echo $file\_blast && \
+    if [ ! -f ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result/$file\_blast ] ; then
+        blastn \
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote && \
+    else 
+        echo "blast is already done" && \
+        echo $file\_blast
+    fi
+mv $file\_blast ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result ; done
 
 
 
