77907433cb767aff7ee7279561b1d45860d02d0d
bridlin <bridlinbarckmann@gmail.com>
2025-02-04 13:33:46 +0100

    parameters

--- run_compare_results.sh
+++ run_compare_results.sh
@@ -14,12 +14,12 @@ module load python/3.9
 
 source WGS_metagenomic_analysis/config.txt
 
-eu_result=kraken2-results_$run\_5prime-trimmed/EuPathDB48
-plus_result=kraken2-results_$run\_5prime-trimmed/PlusPF
+kraken_plus=$output_dir/$kraken_output_dir\/
+kraken_eu=$output_dir/$kraken_output_dir_2\/
 
-echo $eu_result
-echo $plus_result
+echo $kraken_eu
+echo $kraken_plus
 
-python3 WGS_metagenomic_analysis/compare_results.py  $eu_result
+python3 WGS_metagenomic_analysis/compare_results.py  $kraken_eu
 
-python3 WGS_metagenomic_analysis/compare_results.py  $plus_result
+python3 WGS_metagenomic_analysis/compare_results.py  $kraken_plus
