41030e74fe797887931b98bb45658795a085260a
bridlin <bridlinbarckmann@gmail.com>
2025-05-21 14:45:46 +0200

    re_blastin sh to check emplty blasts

---
+++ auto_re-blast.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user b-barckmann@chu-montpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpus-per-task 4
+#SBATCH --mem  128GB
+
+module load blast/2.14.0
+module load python/3.9
+
+source WGS_metagenomic_analysis/config.txt
+
+cd auto_blast_folder/
+
+mkdir ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result
+for files in ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/*.1.fa ; do 
+    file=$( echo $files | cut -d / -f 4) && 
+    # echo $files && 
+    if [ ! -f ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result/$file\_blast ] ; then 
+        echo $file\_blast && 
+        echo "blasting..." &&
+        blastn \
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote 
+    else 
+        if [ ! -s ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result/$file\_blast ]; then
+            echo $file\_blast && 
+            echo "file is there but empty re-blasting..." &&
+            blastn \
+            -db nt \
+            -query $files \
+            -out $file\_blast  \
+            -max_target_seqs 5 \
+            -max_hsps 5   \
+            -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+            -remote 
+        fi
+        echo $file\_blast  && 
+        echo "blast is already done"
+    fi 
+    if [  -f $file\_blast ] ; then 
+        mv $file\_blast ../kraken2-results_$run\_5prime-trimmed/EuPathDB48/blast_result ; fi 
+done
+
+
+
+
+
+mkdir ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result
+for files in ../kraken2-results_$run\_5prime-trimmed/PlusPF/*.1.fa ; do 
+    file=$( echo $files | cut -d / -f 4) && 
+    # echo $files && 
+    if [ ! -f ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result/$file\_blast ] ; then 
+        echo $file\_blast && 
+        echo "blasting..." &&
+        blastn \
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote 
+    else 
+        if [ ! -s ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result/$file\_blast ]; then
+            echo $file\_blast && 
+            echo "file is there but empty re-blasting..." &&
+            blastn \
+            -db nt \
+            -query $files \
+            -out $file\_blast  \
+            -max_target_seqs 5 \
+            -max_hsps 5   \
+            -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+            -remote 
+        fi
+        echo $file\_blast  && 
+        echo "blast is already done"
+    fi 
+    if [  -f $file\_blast ] ; then 
+        mv $file\_blast ../kraken2-results_$run\_5prime-trimmed/PlusPF/blast_result ; fi 
+done
