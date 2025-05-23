3dbc392507e0b96dbc61be9c57ba31aa59caa91f
bridlin <bridlinbarckmann@gmail.com>
2025-05-23 11:02:33 +0200

    file name read extraction

--- read-extraction.sh
+++ read-extraction.sh
@@ -20,6 +20,15 @@ module load python/3.9
 source WGS_metagenomic_analysis/config_readextraction
 
 for sample in "${input_list[@]}"; do for id in "${id_list[@]}"; do 
-python KrakenTools-master/extract_kraken_reads.py -k $output_dir/$db/$sample$db\.kraken2 --include-children -s $read_directory/$sample\nonhuman_reads_dedup_5trimmed.1.fastq -s2 $read_directory/$sample\nonhuman_reads_dedup_5trimmed.2.fastq -t $id\  -r $output_dir/$db/$sample$db\.k2report -o $sample\.tid$id\_all.1.fa  -o2 $sample\.tid$id\_all.2.fa  ; done ; done
+python \
+    KrakenTools-master/extract_kraken_reads.py \
+    -k $output_dir/$db/$sample$db\.kraken2 \
+    --include-children \
+    -s $read_directory/$sample\nonhuman_reads_5trimmed.1.fastq \
+    -s2 $read_directory/$sample\nonhuman_reads_5trimmed.2.fastq \
+    -t $id\  \
+    -r $output_dir/$db/$sample$db\.k2report \
+    -o $sample\.tid$id\_all.1.fa  \
+    -o2 $sample\.tid$id\_all.2.fa  ; done ; done
 
 
