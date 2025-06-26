3b17c81a09789caf4ea911beb0f6a29ea605017d
bridlin <bridlinbarckmann@gmail.com>
2025-06-26 14:30:00 +0200

    changes varaibles run compre analu=ysis

--- run_compare_results.sh
+++ run_compare_results.sh
@@ -14,13 +14,27 @@ module load python/3.9
 
 source WGS_metagenomic_analysis/config.txt
 
+echo "run compare results"
+echo "run= $run"
+echo "input_list= ${input_list[@]}"
+echo "kraken2_E= $kraken2_E"    
+echo "kraken2_P= $kraken2_P"
+
+kraken2_db_E=Kraken2_db/$kraken2_E
+kraken2_db_P=Kraken2_db/$kraken2_P
+
+
+fastq_directory=$run\_fastq
 output_dir=kraken2-results_$run\_5prime-trimmed
-kraken_plus=$output_dir/$kraken_output_dir\/
-kraken_eu=$output_dir/$kraken_output_dir_2\/
+output_dir_E=$output_dir/$kraken2_E
+output_dir_P=$output_dir/$kraken2_P
+ echo "output_dir_E is " $output_dir_E
+ echo "output_dir_P is " $output_dir_P
 
-echo $kraken_eu
-echo $kraken_plus
 
-python3 WGS_metagenomic_analysis/compare_results.py  $kraken_eu
 
-python3 WGS_metagenomic_analysis/compare_results.py  $kraken_plus
+
+
+python3 WGS_metagenomic_analysis/compare_results.py  $output_dir_E\/
+
+python3 WGS_metagenomic_analysis/compare_results.py  $output_dir_P\/
