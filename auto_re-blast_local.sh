289152acfe27407c5a0c155e3dd8d9eb785a1826
bridlin <bridlinbarckmann@gmail.com>
2025-07-03 13:59:42 +0200

    added two local blast script

---
+++ auto_re-blast_local.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user b-barckmann@chu-montpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpus-per-task 4
+#SBATCH --mem  128GB
+
+module load blast/2.14.0
+module load python/3.9
+
+source WGS_metagenomic_analysis/config.txt
+
+cd auto_blast_folder/
+output_dir=kraken2-results_$run\_5prime-trimmed
+output_dir_E=$output_dir/$kraken2_E
+output_dir_P=$output_dir/$kraken2_P
+
+echo "run auto re-blast"
+echo "run= $run"
+echo "input_list= ${input_list[@]}"
+echo "kraken2_E= $kraken2_E"    
+echo "kraken2_P= $kraken2_P"
+echo "output_dir_E is " $output_dir_E
+echo "output_dir_P is " $output_dir_P
+
+mkdir ../$output_dir_E\/blast_result
+mkdir ../$output_dir_P\/blast_result
+
+# for files in ../$output_dir_E\/extracted_reads/*.1.fa ; do 
+#     file=$(basename "$files")   
+#     echo $files 
+#     echo $file
+#     if [ ! -f ../$output_dir_E\/blast_result/$file\_blast ]  
+#     then 
+#         echo $file\_blast 
+#         echo "blasting..." 
+#         blastn \
+#         -db nt \
+#         -query $files \
+#         -out $file\_blast  \
+#         -max_target_seqs 5 \
+#         -max_hsps 5   \
+#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+#         -remote 
+#     elif [ ! -s ../$output_dir_E\/blast_result/$file\_blast ] 
+#     then
+#         echo $file\_blast  
+#         echo "file is there but empty re-blasting..." 
+#         blastn \
+#         -db nt \
+#         -query $files \
+#         -out $file\_blast  \
+#         -max_target_seqs 5 \
+#         -max_hsps 5   \
+#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+#         -remote 
+#     else
+#         echo $file\_blast  
+#         echo "blast is already done" 
+#     fi 
+#     if [  -f $file\_blast ] 
+#     then 
+#         mv $file\_blast ../$output_dir_E\/blast_result  
+#     fi 
+# done
+
+
+
+
+
+
+for files in ../$output_dir_P\/extracted_reads_2/*.1.fa ; do 
+     file=$(basename "$files")  
+    echo $files 
+    echo $file 
+    if [ ! -f ../$output_dir_P\/blast_result/$file\_blast ] 
+    then 
+        echo $file\_blast  
+        echo "blasting..." 
+        blastn \
+        -db ../../bank/nt/current/blast/nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" 
+    elif [ ! -s ../$output_dir_P\/blast_result/$file\_blast ] 
+    then
+        echo $file\_blast 
+        echo "file is there but empty re-blasting..." 
+        blastn \
+        -db ../../bank/nt/current/blast/nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" 
+    else
+        echo $file\_blast  
+        echo "blast is already done" 
+    fi 
+    if [  -f $file\_blast ]  
+    then 
+        mv $file\_blast ../$output_dir_P\/blast_result  
+    fi 
+done
