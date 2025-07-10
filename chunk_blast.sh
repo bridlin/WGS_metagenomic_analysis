6328de5810e1777c6714434c4273464049d34560
bridlin <bridlinbarckmann@gmail.com>
2025-07-10 16:18:00 +0200

    config run 16 and line comment dchunk blast

--- chunk_blast.sh
+++ chunk_blast.sh
@@ -32,7 +32,7 @@ echo "output_dir_P is " $output_dir_P
 
 # batching  the reads for light blasting ito 100 reads per fasta file
 echo "Batching reads for blasting"
-python3 WGS_metagenomic_analysis/batch_extracted_reads.py $output_dir_E $run $kraken2_E
+# python3 WGS_metagenomic_analysis/batch_extracted_reads.py $output_dir_E $run $kraken2_E
 
 # python3 WGS_metagenomic_analysis/batch_extracted_reads.py $output_dir_P $run $kraken2_P
 
@@ -81,42 +81,42 @@ done
 
 
 
-# for files in ../$output_dir_P\/blast_chunks/*.fasta ; do 
-#     file=$(basename "$files")  
-#     # echo $files && 
-#     if [ ! -f ../$output_dir_P\/blast_result/$file\_blast ]  
-#     then 
-#         echo $file\_blast  
-#         echo "blasting..." 
-#         blastn \
-#         -db nt \
-#         -query $files \
-#         -out $file\_blast  \
-#         -max_target_seqs 5 \
-#         -max_hsps 5   \
-#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-#         -remote 
-#     elif [ ! -s ../$output_dir_P\/blast_result/$file\_blast ] 
-#     then
-#         echo $file\_blast  
-#         echo "file is there but empty re-blasting..." 
-#         blastn \
-#         -db nt \
-#         -query $files \
-#         -out $file\_blast  \
-#         -max_target_seqs 5 \
-#         -max_hsps 5   \
-#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-#         -remote  
-#     else 
-#         echo $file\_blast   
-#         echo "blast is already done"
-#     fi 
-#     if [  -f $file\_blast ]  
-#     then 
-#         mv $file\_blast ../$output_dir_P\/blast_result/$file\_blast  
-#     fi 
-# done
+for files in ../$output_dir_P\/blast_chunks/*.fasta ; do 
+    file=$(basename "$files")  
+    # echo $files && 
+    if [ ! -f ../$output_dir_P\/blast_result/$file\_blast ]  
+    then 
+        echo $file\_blast  
+        echo "blasting..." 
+        blastn \
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote 
+    elif [ ! -s ../$output_dir_P\/blast_result/$file\_blast ] 
+    then
+        echo $file\_blast  
+        echo "file is there but empty re-blasting..." 
+        blastn \
+        -db nt \
+        -query $files \
+        -out $file\_blast  \
+        -max_target_seqs 5 \
+        -max_hsps 5   \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote  
+    else 
+        echo $file\_blast   
+        echo "blast is already done"
+    fi 
+    if [  -f $file\_blast ]  
+    then 
+        mv $file\_blast ../$output_dir_P\/blast_result/$file\_blast  
+    fi 
+done
 
 cd ..
 
@@ -124,4 +124,4 @@ dechunking the blast results
 
 python3 WGS_metagenomic_analysis/dechunk_blast_results.py  $output_dir_E\
  
-# python3 WGS_metagenomic_analysis/dechunk_blast_results.py  $output_dir_P\
+python3 WGS_metagenomic_analysis/dechunk_blast_results.py  $output_dir_P\
