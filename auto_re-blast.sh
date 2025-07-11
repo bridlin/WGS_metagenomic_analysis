f7479768edb3dc40a698217f110c8c14ff83ee6a
bridlin <bridlinbarckmann@gmail.com>
2025-07-11 14:31:59 +0200

    output folder name

--- auto_re-blast.sh
+++ auto_re-blast.sh
@@ -16,7 +16,8 @@ module load python/3.9
 source WGS_metagenomic_analysis/config.txt
 
 cd auto_blast_folder/
-output_dir=kraken2-results_$run\_5prime-trimmed_clumped
+# output_dir=kraken2-results_$run\_5prime-trimmed_clumped
+output_dir=kraken2-results_$run\_5prime-trimmed
 output_dir_E=$output_dir/$kraken2_E
 output_dir_P=$output_dir/$kraken2_P
 
