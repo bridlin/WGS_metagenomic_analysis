a7d0b1e9b6f03a005b411441e0b40072d180f9fc
bridlin <bridlinbarckmann@gmail.com>
2025-08-11 14:19:50 +0200

    line command

--- run_complete_analysis.sh
+++ run_complete_analysis.sh
@@ -49,106 +49,106 @@ mkdir $output_dir_P
 
 
 
-# ### run fastqc, cutadapt and trimmomatic on the raw reads
-# echo "run fastqc, cutadapt and trimmomatic on the raw reads"
+### run fastqc, cutadapt and trimmomatic on the raw reads
+echo "run fastqc, cutadapt and trimmomatic on the raw reads"
 
-# for sample in "${input_list[@]}"; do
-# fastqc \
-#     $fastq_directory/$sample\L001_R1_001.fastq.gz \
-#     --outdir $output_dir &&
-# fastqc \
-#     $fastq_directory/$sample\L001_R2_001.fastq.gz \
-#     --outdir $output_dir &&
-# cutadapt  \
-#     -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
-#     -o $fastq_directory/$sample\L001_R1_001_3trimmed.fastq.gz \
-#     -p $fastq_directory/$sample\L001_R2_001_3trimmed.fastq.gz  \
-#     $fastq_directory/$sample\L001_R1_001.fastq.gz  $fastq_directory/$sample\L001_R2_001.fastq.gz \
-#     --minimum-length 40 \
-#     > $output_dir/$sample\all_cutadapt_report.txt &&
-# trimmomatic \
-#     PE \
-#     -threads 4 \
-#     -trimlog $output_dir/$sample\trim \
-#     $fastq_directory/$sample\L001_R1_001_3trimmed.fastq.gz $fastq_directory/$sample\L001_R2_001_3trimmed.fastq.gz \
-#     $fastq_directory/$sample\L001_R1_001_3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R1_001_3trimmed_q20_un.fastq.gz $fastq_directory/$sample\L001_R2_001_3trimmed_q20.fastq.gz  $fastq_directory/$sample\L001_R2_001_3trimmed_q20_un.fastq.gz \
-#     SLIDINGWINDOW:4:20 \
-#     MINLEN:40 &&
-# clumpify.sh \
-#     in1=$fastq_directory/$sample\L001_R1_001_3trimmed_q20.fastq.gz \
-#     in2=$fastq_directory/$sample\L001_R2_001_3trimmed_q20.fastq.gz \
-#     out1=$fastq_directory/$sample\L001_R1_001_3trimmed_q20_clumped.fastq.gz \
-#     out2=$fastq_directory/$sample\L001_R2_001_3trimmed_q20_clumped.fastq.gz \
-#     dedupe=t \
-#     optical=f &&
-# fastqc \
-#     $fastq_directory/$sample\L001_R1_001_3trimmed_q20_clumped.fastq.gz \
-#     --outdir $output_dir &&
-# fastqc \
-#     $fastq_directory/$sample\L001_R2_001_3trimmed_q20_clumped.fastq.gz \
-#     --outdir $output_dir &&
-# bowtie2 \
-#     -x ../../bank/bowtie2/Homo_sapiens.GRCh38.dna.toplevel \
-#     -1 $fastq_directory/$sample\L001_R1_001_3trimmed_q20_clumped.fastq.gz -2 $fastq_directory/$sample\L001_R2_001_3trimmed_q20_clumped.fastq.gz  \
-#     --un-conc $fastq_directory/$sample\nonhuman_reads.fastq \
-#     -S $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam \
-#     2> $output_dir/$sample\_bowtie.log &&
-# samtools \
-#     view -S \
-#     -b $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam  \
-#     > $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
-# samtools \
-#     sort $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam  \
-#     -o $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
-# samtools \
-#     index $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
-# samtools \
-#     reheader -c 'grep -v ^@PG' $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam  \
-#     > $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered.bam &&
-# picard \
-#     CollectInsertSizeMetrics   \
-#     -I $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered.bam  \
-#     -O $output_dir/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered_insert_size_metrics.txt  \
-#     -H $output_dir/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered_insert_size_histogram.pdf  \
-#     -M 0.5  &&
-# rm -f  $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered.bam &&
-# rm -f  $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam &&
-# rm -f  $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
-# cutadapt  \
-#     -g AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -G AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
-#     -o $fastq_directory/$sample\nonhuman_reads_5trimmed.1.fastq  \
-#     -p $fastq_directory/$sample\nonhuman_reads_5trimmed.2.fastq  \
-#     $fastq_directory/$sample\nonhuman_reads.1.fastq  $fastq_directory/$sample\nonhuman_reads.2.fastq \
-#     --minimum-length 40 \
-#     > $output_dir/$sample\nonhuman_reads_cutadapt_report.txt &&
-# kraken2 \
-#     --db $kraken2_db_E \
-#     --threads 8 \
-#     --minimum-hit-groups 3  \
-#     --report-minimizer-data \
-#     --report $output_dir_E/$sample$kraken2_E\.k2report  \
-#     --paired $fastq_directory/$sample\nonhuman_reads_5trimmed.1.fastq $fastq_directory/$sample\nonhuman_reads_5trimmed.2.fastq \
-#     > $output_dir_E/$sample$kraken2_E\.kraken2 &&
-# kraken2 \
-#     --db $kraken2_db_P \
-#     --threads 8 \
-#     --minimum-hit-groups 3  \
-#     --report-minimizer-data \
-#     --report $output_dir_P/$sample$kraken2_P\.k2report  \
-#     --paired $fastq_directory/$sample\nonhuman_reads_5trimmed.1.fastq $fastq_directory/$sample\nonhuman_reads_5trimmed.2.fastq \
-#     > $output_dir_P/$sample$kraken2_P\.kraken2 ; done
+for sample in "${input_list[@]}"; do
+fastqc \
+    $fastq_directory/$sample\L001_R1_001.fastq.gz \
+    --outdir $output_dir &&
+fastqc \
+    $fastq_directory/$sample\L001_R2_001.fastq.gz \
+    --outdir $output_dir &&
+cutadapt  \
+    -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
+    -o $fastq_directory/$sample\L001_R1_001_3trimmed.fastq.gz \
+    -p $fastq_directory/$sample\L001_R2_001_3trimmed.fastq.gz  \
+    $fastq_directory/$sample\L001_R1_001.fastq.gz  $fastq_directory/$sample\L001_R2_001.fastq.gz \
+    --minimum-length 40 \
+    > $output_dir/$sample\all_cutadapt_report.txt &&
+trimmomatic \
+    PE \
+    -threads 4 \
+    -trimlog $output_dir/$sample\trim \
+    $fastq_directory/$sample\L001_R1_001_3trimmed.fastq.gz $fastq_directory/$sample\L001_R2_001_3trimmed.fastq.gz \
+    $fastq_directory/$sample\L001_R1_001_3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R1_001_3trimmed_q20_un.fastq.gz $fastq_directory/$sample\L001_R2_001_3trimmed_q20.fastq.gz  $fastq_directory/$sample\L001_R2_001_3trimmed_q20_un.fastq.gz \
+    SLIDINGWINDOW:4:20 \
+    MINLEN:40 &&
+clumpify.sh \
+    in1=$fastq_directory/$sample\L001_R1_001_3trimmed_q20.fastq.gz \
+    in2=$fastq_directory/$sample\L001_R2_001_3trimmed_q20.fastq.gz \
+    out1=$fastq_directory/$sample\L001_R1_001_3trimmed_q20_clumped.fastq.gz \
+    out2=$fastq_directory/$sample\L001_R2_001_3trimmed_q20_clumped.fastq.gz \
+    dedupe=t \
+    optical=f &&
+fastqc \
+    $fastq_directory/$sample\L001_R1_001_3trimmed_q20_clumped.fastq.gz \
+    --outdir $output_dir &&
+fastqc \
+    $fastq_directory/$sample\L001_R2_001_3trimmed_q20_clumped.fastq.gz \
+    --outdir $output_dir &&
+bowtie2 \
+    -x ../../bank/bowtie2/Homo_sapiens.GRCh38.dna.toplevel \
+    -1 $fastq_directory/$sample\L001_R1_001_3trimmed_q20_clumped.fastq.gz -2 $fastq_directory/$sample\L001_R2_001_3trimmed_q20_clumped.fastq.gz  \
+    --un-conc $fastq_directory/$sample\nonhuman_reads.fastq \
+    -S $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam \
+    2> $output_dir/$sample\_bowtie.log &&
+samtools \
+    view -S \
+    -b $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam  \
+    > $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
+samtools \
+    sort $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam  \
+    -o $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
+samtools \
+    index $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam &&
+samtools \
+    reheader -c 'grep -v ^@PG' $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted.bam  \
+    > $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered.bam &&
+picard \
+    CollectInsertSizeMetrics   \
+    -I $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered.bam  \
+    -O $output_dir/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered_insert_size_metrics.txt  \
+    -H $output_dir/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered_insert_size_histogram.pdf  \
+    -M 0.5  &&
+rm -f  $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel_sorted_reheadered.bam &&
+rm -f  $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam &&
+rm -f  $fastq_directory/$sample\aln-pe_Homo_sapiens.GRCh38.dna.toplevel.sam.bam &&
+cutadapt  \
+    -g AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -G AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
+    -o $fastq_directory/$sample\nonhuman_reads_5trimmed.1.fastq  \
+    -p $fastq_directory/$sample\nonhuman_reads_5trimmed.2.fastq  \
+    $fastq_directory/$sample\nonhuman_reads.1.fastq  $fastq_directory/$sample\nonhuman_reads.2.fastq \
+    --minimum-length 40 \
+    > $output_dir/$sample\nonhuman_reads_cutadapt_report.txt &&
+kraken2 \
+    --db $kraken2_db_E \
+    --threads 8 \
+    --minimum-hit-groups 3  \
+    --report-minimizer-data \
+    --report $output_dir_E/$sample$kraken2_E\.k2report  \
+    --paired $fastq_directory/$sample\nonhuman_reads_5trimmed.1.fastq $fastq_directory/$sample\nonhuman_reads_5trimmed.2.fastq \
+    > $output_dir_E/$sample$kraken2_E\.kraken2 &&
+kraken2 \
+    --db $kraken2_db_P \
+    --threads 8 \
+    --minimum-hit-groups 3  \
+    --report-minimizer-data \
+    --report $output_dir_P/$sample$kraken2_P\.k2report  \
+    --paired $fastq_directory/$sample\nonhuman_reads_5trimmed.1.fastq $fastq_directory/$sample\nonhuman_reads_5trimmed.2.fastq \
+    > $output_dir_P/$sample$kraken2_P\.kraken2 ; done
 
-# multiqc   \
-#     $output_dir \
-#     $output_dir_E \
-#     $output_dir_P \
-#     --outdir $output_dir 
-
-
+multiqc   \
+    $output_dir \
+    $output_dir_E \
+    $output_dir_P \
+    --outdir $output_dir 
 
 
-# mkdir $output_dir_P\/extracted_reads
-# mkdir $output_dir_E\/extracted_reads
+
+
+mkdir $output_dir_P\/extracted_reads
+mkdir $output_dir_E\/extracted_reads
 
 ### run the python script to extract 10 reads per genus from the kraken2 results 1. arguments are the fastq directory and the kraken2 results directory
 echo "run the python script to extract 10 reads per genus from the kraken2 results" 
@@ -160,145 +160,145 @@ python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq_directory $outpu
 
 
 
-# ### batching  the reads for light blasting into 100 reads per fasta file
-# echo "batching  the reads for light blasting into 100 reads per fasta file"
+### batching  the reads for light blasting into 100 reads per fasta file
+echo "batching  the reads for light blasting into 100 reads per fasta file"
 
-# python3 WGS_metagenomic_analysis/batch_extracted_reads.py $output_dir_E  $run $kraken2_E
+python3 WGS_metagenomic_analysis/batch_extracted_reads.py $output_dir_E  $run $kraken2_E
 
-# python3 WGS_metagenomic_analysis/batch_extracted_reads.py $output_dir_P  $run $kraken2_P
+python3 WGS_metagenomic_analysis/batch_extracted_reads.py $output_dir_P  $run $kraken2_P
 
-# ### run blast on the extracted batched reads
-# echo "run blast on the extracted  batched reads"
+### run blast on the extracted batched reads
+echo "run blast on the extracted  batched reads"
 
-# cd auto_blast_folder/
+cd auto_blast_folder/
 
-# mkdir ../$output_dir_E\/blast_result
-# mkdir ../$output_dir_P\/blast_result
-
-
-# # Output log file
-# logfile="../$output_dir_E/blast_${run}_E.log"
-# touch "$logfile"
-
-# echo "=== Starting BLAST run: $(date) ===" >> "$logfile"
-
-# # Loop through all chunked query files
-# for files in ../$output_dir_E/blast_chunks/*.fasta ; do 
-#     file=$(basename "$files")
-#     outfile="${file}_blast"
-#     full_outpath="../$output_dir_E/blast_result/$outfile"
-
-#     # Skip if output already exists
-#     if [ -f "$full_outpath" ]; then 
-#         echo "$outfile - already exists, skipping" | tee -a "$logfile"
-#         continue
-#     fi
-
-#     echo "$outfile - blasting..." | tee -a "$logfile"
-
-#     # Run BLAST and capture stderr to temp file
-#     tmp_stderr=$(mktemp)
-#     blastn -db nt \
-#         -query "$files" \
-#         -out "$outfile" \
-#         -max_target_seqs 5 \
-#         -max_hsps 5 \
-#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-#         -remote 2> "$tmp_stderr"
-
-#     # Check success
-#     exit_code=$?
-#     if [ $exit_code -eq 0 ]; then
-#         if [ -s "$outfile" ]; then
-#             mv "$outfile" "$full_outpath"
-#             echo "$outfile - success" | tee -a "$logfile"
-#         else
-#             echo "$outfile - BLAST completed but file is empty (no hits?)" | tee -a "$logfile"
-#             mv "$outfile" "$full_outpath"
-#         fi
-#     else
-#         echo "$outfile - BLAST FAILED (exit code $exit_code)" | tee -a "$logfile"
-#         echo "--- STDERR ---" >> "$logfile"
-#         cat "$tmp_stderr" >> "$logfile"
-#         echo "--------------" >> "$logfile"
-#         # Optionally: touch empty file so downstream doesn't re-run
-#         touch "$full_outpath.failed"
-#     fi
-
-#     rm "$tmp_stderr"
-# done
-
-# echo "=== Finished BLAST run: $(date) ===" >> "$logfile"
+mkdir ../$output_dir_E\/blast_result
+mkdir ../$output_dir_P\/blast_result
 
 
+# Output log file
+logfile="../$output_dir_E/blast_${run}_E.log"
+touch "$logfile"
 
+echo "=== Starting BLAST run: $(date) ===" >> "$logfile"
 
+# Loop through all chunked query files
+for files in ../$output_dir_E/blast_chunks/*.fasta ; do 
+    file=$(basename "$files")
+    outfile="${file}_blast"
+    full_outpath="../$output_dir_E/blast_result/$outfile"
 
-# # Output log file
-# logfile="../$output_dir_P/blast_${run}_P.log"
-# touch "$logfile"
+    # Skip if output already exists
+    if [ -f "$full_outpath" ]; then 
+        echo "$outfile - already exists, skipping" | tee -a "$logfile"
+        continue
+    fi
 
-# echo "=== Starting BLAST run: $(date) ===" >> "$logfile"
+    echo "$outfile - blasting..." | tee -a "$logfile"
 
-# # Loop through all chunked query files
-# for files in ../$output_dir_P/blast_chunks/*.fasta ; do 
-#     file=$(basename "$files")
-#     outfile="${file}_blast"
-#     full_outpath="../$output_dir_P/blast_result/$outfile"
+    # Run BLAST and capture stderr to temp file
+    tmp_stderr=$(mktemp)
+    blastn -db nt \
+        -query "$files" \
+        -out "$outfile" \
+        -max_target_seqs 5 \
+        -max_hsps 5 \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote 2> "$tmp_stderr"
 
-#     # Skip if output already exists
-#     if [ -f "$full_outpath" ]; then 
-#         echo "$outfile - already exists, skipping" | tee -a "$logfile"
-#         continue
-#     fi
+    # Check success
+    exit_code=$?
+    if [ $exit_code -eq 0 ]; then
+        if [ -s "$outfile" ]; then
+            mv "$outfile" "$full_outpath"
+            echo "$outfile - success" | tee -a "$logfile"
+        else
+            echo "$outfile - BLAST completed but file is empty (no hits?)" | tee -a "$logfile"
+            mv "$outfile" "$full_outpath"
+        fi
+    else
+        echo "$outfile - BLAST FAILED (exit code $exit_code)" | tee -a "$logfile"
+        echo "--- STDERR ---" >> "$logfile"
+        cat "$tmp_stderr" >> "$logfile"
+        echo "--------------" >> "$logfile"
+        # Optionally: touch empty file so downstream doesn't re-run
+        touch "$full_outpath.failed"
+    fi
 
-#     echo "$outfile - blasting..." | tee -a "$logfile"
+    rm "$tmp_stderr"
+done
 
-#     # Run BLAST and capture stderr to temp file
-#     tmp_stderr=$(mktemp)
-#     blastn -db nt \
-#         -query "$files" \
-#         -out "$outfile" \
-#         -max_target_seqs 5 \
-#         -max_hsps 5 \
-#         -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
-#         -remote 2> "$tmp_stderr"
-
-#     # Check success
-#     exit_code=$?
-#     if [ $exit_code -eq 0 ]; then
-#         if [ -s "$outfile" ]; then
-#             mv "$outfile" "$full_outpath"
-#             echo "$outfile - success" | tee -a "$logfile"
-#         else
-#             echo "$outfile - BLAST completed but file is empty (no hits?)" | tee -a "$logfile"
-#             mv "$outfile" "$full_outpath"
-#         fi
-#     else
-#         echo "$outfile - BLAST FAILED (exit code $exit_code)" | tee -a "$logfile"
-#         echo "--- STDERR ---" >> "$logfile"
-#         cat "$tmp_stderr" >> "$logfile"
-#         echo "--------------" >> "$logfile"
-#         # Optionally: touch empty file so downstream doesn't re-run
-#         touch "$full_outpath.failed"
-#     fi
-
-#     rm "$tmp_stderr"
-# done
-
-# echo "=== Finished BLAST run: $(date) ===" >> "$logfile"
+echo "=== Finished BLAST run: $(date) ===" >> "$logfile"
 
 
 
 
-# cd ..
 
-# # dechunking the blast results
-# echo "dechunking the blast results"
+# Output log file
+logfile="../$output_dir_P/blast_${run}_P.log"
+touch "$logfile"
 
-# python3 WGS_metagenomic_analysis/dechunk_blast_results.py  $output_dir_E\
+echo "=== Starting BLAST run: $(date) ===" >> "$logfile"
+
+# Loop through all chunked query files
+for files in ../$output_dir_P/blast_chunks/*.fasta ; do 
+    file=$(basename "$files")
+    outfile="${file}_blast"
+    full_outpath="../$output_dir_P/blast_result/$outfile"
+
+    # Skip if output already exists
+    if [ -f "$full_outpath" ]; then 
+        echo "$outfile - already exists, skipping" | tee -a "$logfile"
+        continue
+    fi
+
+    echo "$outfile - blasting..." | tee -a "$logfile"
+
+    # Run BLAST and capture stderr to temp file
+    tmp_stderr=$(mktemp)
+    blastn -db nt \
+        -query "$files" \
+        -out "$outfile" \
+        -max_target_seqs 5 \
+        -max_hsps 5 \
+        -outfmt "6 qseqid sseqid sscinames pident qcovs qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxids" \
+        -remote 2> "$tmp_stderr"
+
+    # Check success
+    exit_code=$?
+    if [ $exit_code -eq 0 ]; then
+        if [ -s "$outfile" ]; then
+            mv "$outfile" "$full_outpath"
+            echo "$outfile - success" | tee -a "$logfile"
+        else
+            echo "$outfile - BLAST completed but file is empty (no hits?)" | tee -a "$logfile"
+            mv "$outfile" "$full_outpath"
+        fi
+    else
+        echo "$outfile - BLAST FAILED (exit code $exit_code)" | tee -a "$logfile"
+        echo "--- STDERR ---" >> "$logfile"
+        cat "$tmp_stderr" >> "$logfile"
+        echo "--------------" >> "$logfile"
+        # Optionally: touch empty file so downstream doesn't re-run
+        touch "$full_outpath.failed"
+    fi
+
+    rm "$tmp_stderr"
+done
+
+echo "=== Finished BLAST run: $(date) ===" >> "$logfile"
+
+
+
+
+cd ..
+
+# dechunking the blast results
+echo "dechunking the blast results"
+
+python3 WGS_metagenomic_analysis/dechunk_blast_results.py  $output_dir_E\
  
-# python3 WGS_metagenomic_analysis/dechunk_blast_results.py  $output_dir_P\
+python3 WGS_metagenomic_analysis/dechunk_blast_results.py  $output_dir_P\
 
 
 
