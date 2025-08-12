3bc305c3306b44b97f6e1e696d6c9f5eb95bce73
bridlin <bridlinbarckmann@gmail.com>
2025-08-12 13:39:05 +0200

    line comment

--- run_complete_analysis.sh
+++ run_complete_analysis.sh
@@ -52,7 +52,7 @@ mkdir $output_dir_P
 ### run fastqc, cutadapt and trimmomatic on the raw reads
 echo "run fastqc, cutadapt and trimmomatic on the raw reads"
 
-for sample in "${input_list[@]}"; do
+# for sample in "${input_list[@]}"; do
 # fastqc \
 #     $fastq_directory/$sample\L001_R1_001.fastq.gz \
 #     --outdir $output_dir &&
