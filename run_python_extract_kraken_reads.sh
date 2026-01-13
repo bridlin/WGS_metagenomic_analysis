bb8ba58589070fa73c17b4ad5284a53c2d35da06
bridlin <bridlinbarckmann@gmail.com>
2026-01-13 09:46:26 +0100

    updated run_python_extract_kraken_reads and config

--- run_python_extract_kraken_reads.sh
+++ run_python_extract_kraken_reads.sh
@@ -14,16 +14,24 @@ module load python/3.12
 
 source WGS_metagenomic_analysis/config.txt
 
-echo kraken2-results_$run\_5prime-trimmed/PlusPF/
-
-kraken_plus=kraken2-results_$run\_5prime-trimmed/PlusPF/
-kraken_eu=kraken2-results_$run\_5prime-trimmed/EuPathDB48/
-fastq=$run\_fastq
 
 
-echo $kraken_plus
-echo $kraken_eu
+echo "run= $run"
+echo "input_list= ${input_list[@]}"
+echo "kraken2_E= $kraken2_E"    
+echo "kraken2_P= $kraken2_P"
+echo "read1_postfix= $read1_postfix"
+echo "read2_postfix= $read2_postfix"
 
-python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_plus
+fastq_directory=$run\_fastq
+output_dir=kraken2-results_$run\_5prime-trimmed
+output_dir_E=$output_dir/$kraken2_E
+output_dir_P=$output_dir/$kraken2_P
 
-python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq $kraken_eu
+
+### run the python script to extract 10 reads per genus from the kraken2 results 1. arguments are the fastq directory and the kraken2 results directory
+echo "run the python script to extract 10 reads per genus from the kraken2 results" 
+
+python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq_directory $output_dir_P\/
+
+python3 WGS_metagenomic_analysis/auto_read-Extraction.py $fastq_directory $output_dir_E\/
