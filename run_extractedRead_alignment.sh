b92a3ac61cf0ad3bd9ddc8917b4e06321f14a290
bridlin <bridlinbarckmann@gmail.com>
2026-01-29 14:46:33 +0100

    added bowtie index chek and bulit in the script

--- run_extractedRead_alignment.sh
+++ run_extractedRead_alignment.sh
@@ -18,15 +18,36 @@ source WGS_metagenomic_analysis/config-align.txt
 
 echo $read_directory
 echo $inputlist
-echo $genome
+echo $genome_prefix
 
+if [[ ! -f "${genome_prefix}.1.bt2" && ! -f "${genome_prefix}.1.bt2l" ]]; then
+    echo "Bowtie2 index not found for ${genome_prefix}. Building index..."
+    bowtie2-build "$genome_prefix" "$genome_prefix" || {
+        echo "ERROR: bowtie2-build failed"
+        exit 1
+    }
+else
+    echo "Bowtie2 index found for ${genome_prefix}"
+fi
 for x in "${inputlist[@]}"; do
 echo $read_directory\/$x\.1.fa &&
 echo $read_directory\/$x\.2.fa &&
-bowtie2 -x $genome -f -p 8  -1 $read_directory\/$x\.1.fa  -2 $read_directory\/$x\.2.fa  -S $read_directory\/$x\_aligned.sam &&
-samtools view -S -b $read_directory\/$x\_aligned.sam > $read_directory\/$x\_aligned.bam  &&
-samtools sort  $read_directory\/$x\_aligned.bam  -o  $read_directory\/$x\_aligned_sorted.bam  &&
-samtools index $read_directory\/$x\_aligned_sorted.bam   &&
+bowtie2 -x $genome_prefix /
+    -f -p 8  \
+    -1 $read_directory\/$x\.1.fa  \
+    -2 $read_directory\/$x\.2.fa  \
+    -S $read_directory\/$x\_aligned.sam &&
+samtools \
+    view \
+    -S \
+    -b $read_directory\/$x\_aligned.sam \
+    > $read_directory\/$x\_aligned.bam  &&
+samtools \
+    sort  \
+    $read_directory\/$x\_aligned.bam  \
+    -o  $read_directory\/$x\_aligned_sorted.bam  &&
+samtools \
+    index $read_directory\/$x\_aligned_sorted.bam   &&
 rm -f $read_directory\/$x\_aligned.sam &&
 rm -f $read_directory\/$x\_aligned.bam ; done
 
